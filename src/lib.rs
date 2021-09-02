90645f435723f74d779ae18aca62c6788ba7f26a
Yury <yury.yukhananov@tutanota.com>
2021-09-01 23:41:31 -0400

    moore machine + more doc

--- src/lib.rs
+++ src/lib.rs
@@ -64,7 +64,7 @@
 //! is unable to produce coherent reactions because no user data is associated with its state.
 //! The only way to obtain something useful from such a machine is by repeating matching its state.
 //! 
-//! Here is how a machine definition can be expanded:
+//! Here is how a machine definition can be extended:
 //! ```rust,ignore
 //!   // user data associated with state
 //!  struct Count {
@@ -134,11 +134,13 @@
 //! ```
 //! LetterCounter::signal(...) method encapsulates the transition matcher.
 //! ```rust,ignore
-//! from_any_state_transition_handler!(Signal for Machine { ...
+//! from_any_state_transition_handler!(Signal for Machine { // ... 
+//! });
 //! ``` 
 //! is alternative for equivalent notation:
 //! ```rust,ignore
-//! transition_handler!(_ on Signal for Machine { ...
+//! transition_handler!(_ on Signal for Machine { // ... 
+//! });
 //! ``` 
 //! 
 //! Now define the instance:
@@ -161,22 +163,27 @@
 //! ```
 //! One can notice that in this example no code directly refers to current fsm's state 
 //! (although it can be accessed via state() method).
+//! 
 //! I consider current state an implementation detail of machine's black box which does not
 //! provide sufficient information about past, namely it does not make much sense to know what 
 //! current state is out of context of some transition.  
+//! 
 //! There is another way to break the black box though.
 //! Consider that this silly counter needs to distinguish the situation of more than 5 B letters counted
 //! entering a specific state, call it MoreThan5B.
+//! 
 //! One could define it this way:
 //! ```rust,ignore
 //! Started on LetterB => IncB_1 on LetterB => IncB_2 on LetterB... and so forth.
 //! ```
-//! There is an alternative conditional pattern for this scenario :
+//! They call it transition explosion.
+//! 
+//! There is an alternative fork pattern for this scenario :
 //! ```rust,ignore
 //!  fsm!( LetterCounter<Count> [
 //!     Idle on Start => Started,
 //!     _ on LetterA => IncA on Other(char) => OtherAfterA,
-//!     _ on LetterB => ?,
+//!     _ on LetterB => ?, // <-------- 
 //!     _ on Other => IncOther,
 //!     _ on Reset => Idle,
 //! ]);
@@ -220,25 +227,105 @@
 //! ```
 //! This design defines a relaxed machine in sense that it won't change state on signal
 //! no transition for which exists for current state.
-//! This means this scenario is not covered:
+//!
+//! ## Moore State Machine
+//! It's often useful to define an action to be performed upon state entry and/or exit regardless
+//! of a transition leading to the change.
+//! This can be used in common scenarios of initialization, for instance, and can save some code 
+//! repetition.
+//! Below is how the machine's declaration syntax is extended to cover such a case:
 //! ```rust,ignore
-//!  fsm!( Baby [
-//!     Content on GiveCandy => Content on WaitASec => Screaming,
-//!     // or whatever else
-//!     Content on _ => Screaming, // <-- _ won't compile
-//!     // and then what ?
-//!     Screaming on GiveHimCandyAnyway => Content,
-//! ]);
+//! // using the previous example, take the reset code out of Transition handler to Idle State Entry handler
+//! fsm!( LetterCounter<Count> [
+//!   Idle on Start => Started,
+//!   _ on LetterA => IncA on Other(char) => OtherAfterA,
+//!   _ on LetterB => IncB,
+//!   _ on Other => IncOther,
+//!   _ on (Number(u32) | Float(f64)) => NumberSt,
+//!   _ on Reset => Idle,
+//!   ],
+//!   [Idle entry, Start exit]
+//! );
+//! on_state_handler!( Idle entry for LetterCounter {
+//!   data.a_count = 0;
+//!   data.b_count = 0;
+//!   data.other_count = 0;
+//!});
+//! on_state_handler!( Start exit for LetterCounter {
+//! // do sth here
+//!});
 //! ```
-//! Contrary to the above General Failure or Reset scenarios are quite common:
-//! ```rust,ignore
-//! fsm!( SRLatch [
-//!     // ...
-//!     // this signal leads to failure regardless of current state
-//!     _ on SLoRLo => InvalidState 
-//! ]);
-//! ```
+//! This syntax recognizes two words only: 'entry' and 'exit'.
 //! 
+//! # Pushdown Automaton
+//! introduces a third variable to the equation which is a stack which, combined with a state and 
+//! input signal presents a model of taking transitions considerably more powerful than a regular state machine.
+//! It is capable of parsing nested patterns, for example. 
+//! Note that according to formal definition signal and stack symbol alphabets do not necesarily coincide.
+//!
+//! In this crate the stack is implemented as a standard Vec with no encapsulation while the formal definition
+//! for PDA stack can be more restrictive as for permitted stack operations.
+//! So here is the declaration example:
+//! 
+//! 
+//! ```rust,ignore
+//!     fsm!( LittleParser<ParserCtx, StackCtx> [ // ...
+//! // state context ---------^          ^---------------- stack context (impls PartialEq) 
+//!     // ...
+//!     ]);
+//! ```
+//! Once declared so, the 'stack' mutable reference becomes accessible in transition handlers.
+//! This stack can be polled, pushed and however manipulated to determine conditional forks on
+//! state transitions.
+//! 
+//! As an experiment I introduced an extended syntax for transition signatures in order to pursuit
+//! for deterministic automaton for some common patterns which for the moment are:
+//! 
+//! 'cmp_pop expr' - stands for compare the stack's top with the given expression and if true, pop it;
+//!
+//! 'cmp_pop_if_last expr' - the same as above but applies only when the stack contains only one item;
+//!
+//! 'peek_cmp expr' - compare the stack's top element with the given expression without modifying the stack;
+//!
+//! 'pop' - pop from the stack unconditionally upon transition;
+//!
+//! 'empty' - condition transition with stack emptiness;
+//!
+//! '!empty' - the negation of the above;
+//! 
+//!  Using this syntax one can introduce a palette of determenistic transitions under more complex conditions
+//!  which are encoded directly in transition signature rather than in the code.
+//!
+//!  For example:
+//! 
+//! ```rust,ignore
+//!     fsm!( LittleParser<ParserCtx, StackCtx> [ 
+//!         // code omitted ...
+//!         // TextSegment is a State, RightBracket is a current symbol delivered with a Signal
+//!         // and (Left)Bracket is a symbol that can possibly be on top of the Stack...
+//!         // if so recognize it as a valid pattern and transtion to the same State 
+//!         // then stack popping can be performed in the handler    
+//!         TextSegment on RightBracket peek_cmp Bracket 
+//!         // next possible situation is that we have the same State and Signal, but the Stack contains
+//!         // other symbol so treat this as an unbalanced segment
+//!         => TextSegment on RightBracket => InvalidSegment, 
+//!         // more code omitted ...
+//!   ]);
+//! ```
+//!  So we have here two distinct deterministic transitions instead of a conditional fork:
+//! ```rust,ignore
+//! transition_handler!(TextSegment on RightBracket peek_cmp Bracket for LittleParser {
+//! // ...
+//! });
+//! transition_handler!(TextSegment on RightBracket for LittleParser {
+//! // ...
+//! });
+//! ```
+//!  
+//! The internal matching for is ordered in a way that more specific 
+//!  patterns are preferred over the more common ones so in the above example the first condition is checked
+//!  first regardless of its visual location in the machine's declaration (at least this was my intention).
+//!  
 //! ## meta_iter Feature
 //! 
 //! The crate defines a feature "meta_iter" which, when is on brings a machine's iterator into scope.
@@ -246,7 +333,11 @@
 //! resulting from waste product of declarations of machine's enums.
 //! This feature brings a bit of text to the code and is only useful for enumerating transitions
 //! while debugging, for instance.
-//! The result could be then sent to a graph visualization software or just printed out.   
+//! The result could be then sent to a graph visualization software or just printed out.
+//! 
+//! For now comprehensive output can be obtained for an FSM only, a PDA will yield the same
+//! output as if it was an FSM.
+//!  
 //! It works like so:
 //! ```rust,ignore
 //! let mut chains = LetterCounter::chains();
@@ -270,7 +361,7 @@
 //! For example for MyMachine the string is accessed by 
 //! 
 //! ```rust,ignore
-//! crate::MY_MACHINE_GEN_CODE 
+//! MY_MACHINE_GEN_CODE 
 //! ```
 //! constant.
 //! 
@@ -370,12 +461,7 @@ pub fn transition_handler(input: proc_macro::TokenStream) -> proc_macro::TokenSt
 pub fn conditional_handler(input: proc_macro::TokenStream) -> proc_macro::TokenStream  {
   quote_transition_handler(input, HandlerSignature::Conditional)
 }
-/*
-#[proc_macro]
-pub fn stackful_conditional_handler(input: proc_macro::TokenStream) -> proc_macro::TokenStream  {
-  quote_transition_handler(input, HandlerSignature::Stackful)
-}
-*/
+
 #[proc_macro]
 pub fn from_any_state_transition_handler(input: proc_macro::TokenStream) -> proc_macro::TokenStream  {
   let mut input1 = TokenStream::from((Ident::new("_", Span::call_site())).into_token_stream());
@@ -383,6 +469,11 @@ pub fn from_any_state_transition_handler(input: proc_macro::TokenStream) -> proc
   input1.extend(TokenStream::from((Ident::new("on", Span::call_site())).into_token_stream()));
   input1.extend(TokenStream::from(input));
   quote_transition_handler(input1.into(), HandlerSignature::Deterministic)
+}
+
+#[proc_macro]
+pub fn on_state_handler(input: proc_macro::TokenStream) -> proc_macro::TokenStream  {
+  quote_state_entry_handler(input)
 }
 
 enum HandlerSignature {
@@ -453,6 +544,8 @@ struct MatchTransitionEntry {
   input: Option<Type>,
   stack_expr: StackOp,
   to_state: Option<Ident>,
+  state_entry: bool,
+  state_exit: bool,
 }
 
 impl std::cmp::PartialOrd for EnumIdentAcceptingAny {
@@ -541,7 +634,24 @@ impl ToTokens for MatchTransitionEntry {
     let cb_token = &callback_ident(state, signal, stack_expr);
     let data_arg = quote! { &mut self.data };
     let stack_arg = quote! { &mut self.stack };
-    let return_quoted = if let Some(ref to) = self.to_state {quote! {; #to}} else { quote! {} };
+    
+    let state_exit_handler = if self.state_exit {
+      let cb_exit_ident = &state_exit_callback(state);
+      quote! { (Self::#cb_exit_ident)(#data_arg, #stack_arg); }
+    } else { quote! {} };
+
+    let return_quoted 
+      = if let Some(ref to) = self.to_state {
+        let cb_entry_ident = &state_entry_callback(to);
+        
+        let state_entry_handler 
+          = if self.state_entry {quote! { (Self::#cb_entry_ident)(#data_arg, #stack_arg) }} 
+            else {quote! {}};
+
+        quote! {; #state_entry_handler; #to}
+    } else { 
+      quote! {} 
+    };
     let pop_match_guard 
       = match stack_expr {
         StackOp::CompareAndPop(expr) => quote! {if if let Some(val) = self.stack.last() {
@@ -561,10 +671,12 @@ impl ToTokens for MatchTransitionEntry {
 
     let quoted =
       if let Some(_) = self.input {
-        quote! { (#state, #signal(input)) #pop_match_guard => { (Self::#cb_token)(input, #data_arg, #stack_arg) #return_quoted } } 
+        quote! { (#state, #signal(input)) #pop_match_guard 
+          => { #state_exit_handler (Self::#cb_token)(input, #data_arg, #stack_arg) #return_quoted } } 
   
       } else {     
-        quote! { (#state, #signal) #pop_match_guard  => { (Self::#cb_token)(#data_arg, #stack_arg) #return_quoted } }
+        quote! { (#state, #signal) #pop_match_guard  
+          => { #state_exit_handler (Self::#cb_token)(#data_arg, #stack_arg) #return_quoted } }
       };
   
     quoted.to_tokens(tokens);
@@ -625,6 +737,14 @@ fn callback_ident(state: &Ident, signal: &Ident, stack_expr: &StackOp) -> Ident 
       pop_expr_conv.to_case(Case::Snake)),
       Span::call_site()
   )
+}
+fn state_entry_callback(state: &Ident) -> Ident {
+  Ident::new(
+    &format!("{}_entry", state.to_string().to_case(Case::Snake)), Span::call_site())
+}
+fn state_exit_callback(state: &Ident) -> Ident {
+  Ident::new(
+    &format!("{}_exit", state.to_string().to_case(Case::Snake)), Span::call_site())
 }
 
 struct TransitionHandlerArgsParser {
@@ -730,6 +850,8 @@ impl Parse for FSMParser {
                   input: input_type.clone(),
                   stack_expr: stack_expr.clone(),
                   to_state: to_state.clone(),
+                  state_entry: false,
+                  state_exit: false,
                 });
 
                 if let EnumIdentAcceptingAny::SomeVariant(ref from) = from_state {
@@ -757,7 +879,34 @@ impl Parse for FSMParser {
         if state_enum_tokens.len() == 0 {
           panic!("Stateless State Machine ???");
         }
-        let match_transition_tokens = match_transition_tokens.into_sorted_vec();
+        let mut match_transition_tokens = match_transition_tokens.into_sorted_vec();
+        
+        if !input.is_empty() {
+          input.parse::<Token![,]>()?;
+          let content;
+          bracketed!(content in input);
+          while !content.is_empty() {
+            let state_entry_handler: StateEntryDecl = content.parse()?;
+            match_transition_tokens
+              .iter_mut()
+              .filter(|tr| tr.to_state.as_ref() == Some(&state_entry_handler.state))
+              .for_each(|tr| {
+                tr.state_entry |= state_entry_handler.is_entry;
+//                tr.state_exit |= !state_entry_handler.is_entry;
+              });
+            match_transition_tokens
+              .iter_mut()
+              .filter(|tr| tr.from_state.unwrap_as_ref() == &state_entry_handler.state)
+              .for_each(|tr| {
+//              tr.state_entry |= state_entry_handler.is_entry;
+                tr.state_exit |= !state_entry_handler.is_entry;
+            });
+            if !content.is_empty() {
+                content.parse::<Token![,]>()?;
+            }
+          }
+        }
+
         let _quoted = quote! {
           #(#match_transition_tokens)*,
         };
@@ -942,6 +1091,69 @@ fn quote_transition_handler(input: proc_macro::TokenStream, signature: HandlerSi
         stack: &mut Vec<<Self as #trait_ident>::StackSymbolItem>,
       ) -> #return_type
         #body_block
+    }
+  };
+  (TokenStream::from(tr_quoted.into_token_stream())).into()
+}
+
+struct StateEntryDecl {
+//  fsm_ident: Ident,
+  state: Ident,
+  is_entry: bool,
+}
+impl Parse for StateEntryDecl {
+  fn parse(input: ParseStream) -> Result<Self> {
+    let state: Ident 
+      = if let Ok(ty) = input.parse::<Type>() {
+        match ty {
+          Type::Path(path) => path.path.segments.last().unwrap().ident.clone(),
+          _ => input.parse()?
+        }
+      } else {
+        input.parse()?
+    };
+
+    Ok(Self { 
+      state,
+      is_entry: { 
+        let ident = input.parse::<Ident>()?;
+        if ident != "entry" && ident != "exit" {panic!("'entry' or 'exit' expected here");}
+        ident == "entry"
+      }
+    })
+  }
+}
+struct StateEntrySignature {
+  decl_head: StateEntryDecl,
+  fsm_ident: Ident,
+  body_item: Block,
+}
+
+impl Parse for StateEntrySignature {
+  fn parse(input: ParseStream) -> Result<Self> {
+    Ok(Self { 
+      decl_head: input.parse::<StateEntryDecl>()?,
+      fsm_ident: {input.parse::<Token![for]>()?; input.parse()? },
+      body_item: input.parse()?,
+
+    })
+  }
+}
+  
+fn quote_state_entry_handler(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+	let entry_handler_parsed = parse_macro_input!(input as StateEntrySignature);
+  let fsm_ident = &entry_handler_parsed.fsm_ident;
+  let trait_ident = &trait_ident(fsm_ident);
+  let state_ident = &entry_handler_parsed.decl_head.state;
+  let body_block = &entry_handler_parsed.body_item;
+  let handler_ident = &if entry_handler_parsed.decl_head.is_entry { state_entry_callback(state_ident) } else { state_exit_callback(state_ident) };
+
+  let tr_quoted = quote! {
+    impl #fsm_ident {
+      fn #handler_ident( 
+        data: &mut <Self as #trait_ident>::DataItem,
+        stack: &mut Vec<<Self as #trait_ident>::StackSymbolItem>,
+      ) #body_block
     }
   };
   (TokenStream::from(tr_quoted.into_token_stream())).into()
